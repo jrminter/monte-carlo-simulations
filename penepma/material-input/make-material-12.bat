4b3acbf66f63012074eace324a9c4ddb21e97ac8
John Minter <jrminter@gmail.com>
2018-07-30 17:05:30 -0400

    added material input files

---
+++ penepma/material-input/make-material-12.bat
@@ -0,0 +1,4 @@
+REM make sure input is right before running...
+C:
+cd C:\Users\jrminter\Documents\work\penepma12\pendbase
+REM material.exe < Orthoclase.inp
---
+++ penepma/material-input/make-material-16.bat
@@ -0,0 +1,4 @@
+REM make sure input is right before running...
+C:
+cd C:\Users\jrminter\Documents\work\penepma16\pendbase
+REM material.exe < Orthoclase.inp
