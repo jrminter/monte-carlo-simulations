bb24d64376b3d4e044510c10ccbd43d7caae90ce
John Minter <jrminter@gmail.com>
2018-08-07 19:06:49 -0400

    switch

--- penepma/penepma16/20-nm-C-on-ZnO/C-on-ZnO-on-SiO2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf *.dat
-rm -rf *.rep
-echo "running an 8 hr penepma simulation..."
-penepma16 < "C-on-ZnO-on-SiO2.in"
-
-sleep 2
--- penepma/penepma16/20-nm-C-on-ZnO/cp-files.bat
+++
@@ -1,8 +0,0 @@
-echo "Currently set for 20-nm-C-ZnO-dif-MSIMPA"
-C:
-cd "C:/Users/jrminter/Desktop/test-plt"
-del /F /Q *.dat
-cp C:\Users\jrminter\Documents\work\penepma16\20-nm-C-ZnO-dif-MSIMPA\pe-intens-01.dat pe-intens-01.dat
-cp C:\Users\jrminter\Documents\work\penepma16\20-nm-C-ZnO-dif-MSIMPA\pe-spect-01.dat pe-spect-01.dat
-
-pause
