f736643dc93b42f6f5da3745fbcdd90da0fc136d
John Minter <jrminter@gmail.com>
2020-01-13 09:34:00 -0500

    added penepma12 sims of ZnO and Si at 5kV

---
+++ penepma/penepma12/Si-5kV/Si.bat
@@ -0,0 +1,4 @@
+C:
+rem make sure you want to run this
+rem cd "C:\Users\johnr\Documents\work\penepma12\Si_5kV"
+rem Penepma12 < "Si.in"
---
+++ penepma/penepma12/ZnO-5kV/ZnO.bat
@@ -0,0 +1,7 @@
+C:
+rem make sure you really want to run this...
+rem cd "C:/Users/johnr/Documents/git/monte-carlo-simulations/penepma/penepma12/ZnO-5kV"
+rem Penepma12 < "ZnO.in"
+
+pause
+
